9069bb66f2fe47b8ca044f4380cf91b9102e46a3
Vivek yadav <vivekyadav2382@gmail.com>
2024-09-11 18:57:08 +0530

    add new files

--- python_06.py
+++ python_06.py
@@ -1,1 +1,39 @@
-print("hello world")
+import torch
+from torch import nn
+import requests
+import zipfile
+from pathlib import Path
+import os
+import random 
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Setup device-agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device
+
+# Setup path to a data folder
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it...
+if image_path.is_dir():
+    print(f"{image_path} directory already exists... skipping download")
+else: 
+    print(f"{image_path} does not exist, creating one...")
+    image_path.mkdir(parents=True, exist_ok=True)
+
+# Download pizza, steak and suhsi data
+with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+    request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+    print("Downloading pizza, steak, suhsi data...")
+    f.write(request.content)
+
+# Unzip pizza, steak, sushi data
+with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+    print("Unzipping pizza, steak and sushi data...")
+    zip_ref.extractall(image_path)
